16abb5d226199bb83e5dd14988cfa845ba275ce3
pierrot10 <pierrot@smart-idea.io>
2022-07-17 23:47:06 +0200

    Update

--- ecosensors/console/views.py
+++ ecosensors/console/views.py
@@ -31,9 +31,16 @@ class IndexView(generic.ListView):
 
 def stations(request, fields_id_field):
     #stations_list = get_object_or_404(Stations, pk=field_id_field)
+
+
     stations_list = Stations.objects.filter(fields_id_field=fields_id_field, station_active=1)
     print(stations_list)
     sensors = Sensors.objects.filter(sensor_active=1)
+
+    #s = Sensors.objects.filter(Stations__station_longname)
+    #s = Stations.objects.filter(sensors__sensor_longname)
+    #s = Sensors.stations_id_station.station_longname(fields_id_field=fields_id_field)
+
     return render(request, 'console/stations.html', {'stations_list':stations_list, 'sensors':sensors })
 
 def sensors(request, idstation):
@@ -44,3 +51,37 @@ def sensors(request, idstation):
 #class StationView(generic.DetailView):
 #    model = Stations
 #    template_name = 'console/station.html'
+
+'''
+from datetime import date
+
+from django.db import models
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField(default=date.today)
+    authors = models.ManyToManyField(Author)
+    number_of_comments = models.IntegerField(default=0)
+    number_of_pingbacks = models.IntegerField(default=0)
+    rating = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.headline
+'''
